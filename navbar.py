c4910369c4d544dc3feef5421b80eb536c1e8670
TejwantKaur <kaurtejwant998@gmail.com>
2026-05-20 03:51:56 +0530

    initial commit

--- navbar.py
+++ navbar.py
@@ -1,20 +1,69 @@
+# import streamlit as st
+
+
+# def navbar():
+#     col1, col2, col3 = st.columns(3)
+    
+#     with col1:
+#         if st.button("Home", use_container_width=True):
+#             st.session_state.page = "Home"
+#             st.rerun()
+
+#     with col2:
+#         if st.button("Kids", use_container_width=True):
+#             st.session_state.page = "Kids"
+#             st.rerun()
+
+#     with col3:
+#         if st.button("TV", use_container_width=True):
+#             st.session_state.page = "TV"
+#             st.rerun()
+
+import streamlit.components.v1 as components
+from utils.load_css import load_css
+
 import streamlit as st
 
-
 def navbar():
-    col1, col2, col3 = st.columns(3)
-    
+    css = load_css([
+        "assets/home_styles/navbar.css"
+    ])
+
+    st.markdown(
+        f"<style>{css}</style>",
+        unsafe_allow_html=True
+    )
+    current_page = st.session_state.page
+
+    col1, col2, col3, col4 = st.columns([4,1,1,1])
+
     with col1:
-        if st.button("Home", use_container_width=True):
-            st.session_state.page = "Home"
-            st.rerun()
+        st.markdown("""
+        <h1 style='color:white; margin-top:-10px;'>
+            ✍️ AIGen Write
+        </h1>
+        """, unsafe_allow_html=True)
 
     with col2:
-        if st.button("Kids", use_container_width=True):
-            st.session_state.page = "Kids"
+        if st.button("Home",
+                    use_container_width=True,
+                    type="primary" if current_page == "home" else "secondary"
+                    ):
+            st.session_state.page = "home"
             st.rerun()
 
     with col3:
-        if st.button("TV", use_container_width=True):
-            st.session_state.page = "TV"
+        if st.button("Kids", 
+                    use_container_width=True,
+                    type="primary" if current_page == "kids" else "secondary"
+                    ):
+            st.session_state.page = "kids"
+            st.rerun()
+
+    with col4:
+        if st.button("Smart TV", 
+                    use_container_width=True,
+                    type="primary" if current_page == "tv" else "secondary"
+                    ):
+            st.session_state.page = "tv"
             st.rerun()
--- views/home.py
+++ views/home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from components.hero import hero
 
 def render_home():
-    st.title("🏠 Home Page")
+    # st.title("🏠 Home Page")
+    hero()
